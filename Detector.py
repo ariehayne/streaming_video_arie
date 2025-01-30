547a4c4dc1153bc1a6a9061f8b1c919fdb82919f
ariehayne <arie.hayne@gmail.com>
2025-01-30 15:33:26 +0200

    add Detector.py

---
+++ Detector.py
@@ -0,0 +1,30 @@
+import numpy as np
+from skimage import color
+
+
+class Detector:
+    def __init__(self, queue, output_queue, streamer):
+        self.queue = queue
+        self.output_queue = output_queue
+        self.streamer = streamer
+        self.running = True
+
+    def detect_motion(self):
+        prev_frame = None
+        while self.running:
+            if self.queue:
+                frame, timestamp = self.queue.pop(0)
+                gray = color.rgb2gray(frame)
+
+                if prev_frame is None:
+                    prev_frame = gray
+                    continue
+
+                motion_mask = np.abs(gray - prev_frame) > 0.1
+                prev_frame = gray
+
+                self.output_queue.append((frame, motion_mask, timestamp))
+            elif not self.streamer.running:
+                self.running = False
+        print('Detector is done')
+        self.output_queue.append((None, None, None))
--- Streamer.py
+++ Streamer.py
@@ -1,9 +1,11 @@
 import cv2
+import time
 
 class Streamer:
     def __init__(self, video_path, queue):
         self.video_path = video_path
         self.queue = queue
+        self.running = True
 
     def stream(self):
         cap = cv2.VideoCapture(self.video_path)
@@ -11,5 +13,7 @@ class Streamer:
             ret, frame = cap.read()
             if not ret:
                 break
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert OpenCV BGR to RGB format
             self.queue.append((frame, time.time()))
         cap.release()
+        self.running = False
